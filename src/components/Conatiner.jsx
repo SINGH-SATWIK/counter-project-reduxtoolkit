f0e570f0470a59db60238ed72834dd1eb1c08569
SINGH-SATWIK <119724007+SINGH-SATWIK@users.noreply.github.com>
2024-03-14 21:57:55 +0530

    Add files via upload

---
+++ src/components/Conatiner.jsx
@@ -0,0 +1,11 @@
+const Conatiner=({children})=>{
+    return (
+        <div className="card" style={{width: "70%"}}>
+            <div className="card-body">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Conatiner;
